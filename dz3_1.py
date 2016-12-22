36a8c2e384ea9379a58dceb9250fc58e3c2c1f6f
vnyzaica <pavel.sch.nik@gmail.com>
2016-12-22 20:35:37 +0300

    +1

--- dz3_1.py
+++ dz3_1.py
@@ -1,4 +1,4 @@
-
+import math
 
 
 def arithmetic (a,b,c):
@@ -16,13 +16,34 @@ def arithmetic (a,b,c):
                 else:
         	        print("Неизвастная операция")
     print(x)
-x = int(0)	
-a = int(input("Введите число 1"))
-b = int(input("Введите число 2"))
-c = input("Введите знак")
 
-arithmetic(a,b,c)
-print(x)
+	
+	
+def year(year):
+    if year % 4 == 0 
+	    print("Високосный")
+    else:
+        print("не высокосный")	
+		
+		
 
 
+
+def square(x):
+    a = x * 4
+    b = x * x
+    c = x * math.sqrt(2)
+
+    
+def moon(month)
+    if x ==12 or x <=2
+        print("Зима")
+    else:
+        if x <=5
+		    print("Весна")
+        else:
+            if x <= 8 
+			    print(Лето)
+            else:
+                print("Осень")
     
